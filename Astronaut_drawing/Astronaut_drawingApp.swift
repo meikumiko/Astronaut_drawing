dbbc60128ea9a82f40b04fd8cf50cb11d8efa3af
Hannah Chi <cremi468@gmail.com>
2022-04-26 14:54:54 +0800

    Initial Commit

---
+++ Astronaut_drawing/Astronaut_drawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Astronaut_drawingApp.swift
+//  Astronaut_drawing
+//
+//  Created by Hsiao-Han Chi on 2022/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Astronaut_drawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
